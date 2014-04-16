5f56fb95985e804900e97ec288fa12cd728ef04b
Nicholas Niro <nik_89@xroutine.net>
2014-04-16 04:33:16 -0400

    tools: Added helper code to get and set Console sizes.
    This is strictly for testing/experimentation purposes.

---
+++ src/tools/getWSize.c
@@ -0,0 +1,54 @@
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> /* memset strcmp */
+
+#include <X11/Xlib.h>
+
+int main()
+{
+	struct winsize wsize;
+	char *term = NULL;
+	/* we fetch the window size for the current pty */
+	ioctl(0, TIOCGWINSZ, &wsize);
+
+
+	printf("PTY size : row %d col %d pixels : %dx%d\n"
+		,wsize.ws_row 
+		,wsize.ws_col
+		,wsize.ws_xpixel
+		,wsize.ws_ypixel
+		);
+
+	term = getenv("TERM");
+
+	if (!strcmp("xterm", term)) /* X11 pty */
+	{
+		Display *dpy = NULL;
+		char *cwId = NULL;
+		Window w = 0;
+		unsigned int width = 0, height = 0;
+
+		cwId = getenv("WINDOWID");
+
+		dpy = XOpenDisplay(NULL);
+
+		w = atoi(cwId);
+
+		{
+			Window wroot;
+			int x, y;
+			unsigned int border, depth;
+
+			XGetGeometry(dpy, w, &wroot, &x, &y, &width, &height, &border, &depth);
+
+			printf("size of the window : %dx%d\n", width, height);
+		}
+
+		XCloseDisplay(dpy);
+
+	}
+
+	return 0;
+}
---
+++ src/tools/setWSize.c
@@ -0,0 +1,84 @@
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> /* memset strcmp */
+
+#include <X11/Xlib.h>
+
+int
+convRowToPixel(int row)
+{
+	return (row - 1) * 15 + 19;
+}
+
+int
+convColToPixel(int col)
+{
+	return (col - 1) * 9 + 32;
+}
+
+int main()
+{
+	struct winsize wsize = {37, 100, 0, 0};
+	char *term = NULL;
+	/* struct winsize wsize = {24, 80, 0, 0}; */
+
+	/* environment variables : TERM */
+	term = getenv("TERM");
+
+	printf("trying to set PTY to size : row %d col %d pixels : %dx%d\n"
+		,wsize.ws_row 
+		,wsize.ws_col
+		,wsize.ws_xpixel
+		,wsize.ws_ypixel
+		);
+
+	ioctl(0, TIOCSWINSZ, &wsize);
+
+	memset(&wsize, 0, sizeof(struct winsize));
+
+	/* we fetch the window size for the current pty */
+	ioctl(0, TIOCGWINSZ, &wsize);
+
+	printf("PTY result -- size : row %d col %d pixels : %dx%d\n"
+		,wsize.ws_row 
+		,wsize.ws_col
+		,wsize.ws_xpixel
+		,wsize.ws_ypixel
+		);
+
+	printf("Terminal type : %s\n", term);
+
+	if (!strcmp("xterm", term)) /* X11 pty */
+	{
+		Display *dpy = NULL;
+		char *cwId = NULL;
+		Window w = 0;
+		unsigned int width = 0, height = 0;
+
+		cwId = getenv("WINDOWID");
+
+		dpy = XOpenDisplay(NULL);
+
+		w = atoi(cwId);
+
+		XResizeWindow(dpy, w, convColToPixel(wsize.ws_col), convRowToPixel(wsize.ws_row));
+		
+		{
+			Window wroot;
+			int x, y;
+			unsigned int border, depth;
+
+			XGetGeometry(dpy, w, &wroot, &x, &y, &width, &height, &border, &depth);
+
+			printf("new size of the window : %dx%d -- got %dx%d\n", 
+				convColToPixel(wsize.ws_col), 
+				convRowToPixel(wsize.ws_row), 
+				width, height);
+		}
+
+		XCloseDisplay(dpy);
+	}
+
+	return 0;
+}
